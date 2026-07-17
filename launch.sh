5bf33a9804081f8322e41880c200df7eb34febeb
tokyqwanteos <toky.qwanteos@gmail.com>
2026-07-17 09:03:24 +0300

    Suppression des fichiers hors ligne

--- launch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "========================================"
-echo "   MGA Finance Tracker - Mode Hors Ligne"
-echo "========================================"
-echo ""
-
-python3 launch_offline.py
